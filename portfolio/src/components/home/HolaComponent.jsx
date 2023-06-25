778c7ccc6ef5c8ad7db284fb717ff3467ff0eb8b
Sergio Gonzalez <Sergiomanuel2020@gmail.com>
2023-06-25 16:09:37 -0600

    Creacion del Proyecto y rutas principales

--- portfolio/src/components/home/HolaComponent.jsx
+++ portfolio/src/components/home/HolaComponent.jsx
@@ -1,26 +1,26 @@
 export default function HolaComponent() {
     return (
-        <div class="container-fluid">
-        <div class="row justify-content-center align-items-center">
-          <div class="col-md-12 text-center">
-            <div class="animation-container">
+        <div className="container-fluid">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-12 text-center">
+            <div className="animation-container">
               <div
                 id="hello"
-                class="display-1 font-weight-bold animate__animated animate__fadeInDown"
+                className="display-1 font-weight-bold animate__animated animate__fadeInDown"
               >
                 Hello!
               </div>
               <h2
-                class="display-4 font-weight-bold animate__animated animate__fadeInDown"
+                className="display-4 font-weight-bold animate__animated animate__fadeInDown"
               >
                 My name is Sergio.
               </h2>
-              <div class="description animate__animated animate__fadeInDown">
-                <i class="fas fa-quote-left"></i>
+              <div className="description animate__animated animate__fadeInDown">
+                <i className="fas fa-quote-left"></i>
                 <p>
                 ... and I develop the unthinkable!
                 </p>
-                <i class="fas fa-quote-right"></i>
+                <i className="fas fa-quote-right"></i>
               </div>
             </div>
           </div>
--- portfolio/src/components/home/HolaTools.jsx
+++ portfolio/src/components/home/HolaTools.jsx
@@ -1,26 +1,26 @@
 export default function HolaTools() {
     return (
-        <div class="container-fluid">
-        <div class="row justify-content-center align-items-center">
-          <div class="col-md-6 animate__animated animate__fadeInLeft">
+        <div className="container-fluid">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-6 animate__animated animate__fadeInLeft">
             {/* <img
               src="./assets/img/61808340-854c-4a1d-af62-411cb4d610a5.jpg"
               alt="Sergio's Picture"
             />  */}
           </div>
-          <div class="col-md-6 animate__animated animate__fadeInRight">
-            <div class="slogan-container">
-              <h1 class="display-1 font-weight-bold">
+          <div className="col-md-6 animate__animated animate__fadeInRight">
+            <div className="slogan-container">
+              <h1 className="display-1 font-weight-bold">
                 Imagine It, I'll Create It
               </h1>
-              <div class="buttons">
+              <div className="buttons">
                 <a
                   href="/Projects"
-                  class="btn btn-primary btn-lg"
+                  className="btn btn-primary btn-lg"
                   >Projects</a
                 >
-                <a href="/About" class="btn btn-secondary btn-lg">About</a>
-                <a href="/Contact" class="btn btn-tersiary btn-lg">Contact</a>
+                <a href="/About" className="btn btn-secondary btn-lg">About</a>
+                <a href="/Contact" className="btn btn-tersiary btn-lg">Contact</a>
               </div>
             </div>
           </div>
